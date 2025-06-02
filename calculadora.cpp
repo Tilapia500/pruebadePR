28e7393c1e83fd97bd5b54256308087c7a58e229
SOMBRA-X <jeyfrethcarballoballea@gmail.com>
2025-06-01 18:04:11 -0600

    cosa

--- calculadora.cpp
+++ calculadora.cpp
@@ -1,38 +1,71 @@
+//comentario eduardo
+//comentario jeyfreth
+//comentario ivan
+
+
+/* Códigos ANSI de los Colores Seleccionados
+\033[3 → Celeste
+\033[7 → Blanco
+\033[4 → Rojo
+\033[2 → Verde
+Reset del color al final:
+\033[7 → Restaura el color y estilo original del texto.
+*/
 #include <iostream>
+#include <windows.h>
+#include<string>
 using namespace std;
-//sin poder hacer nada
-int main() {
-    double num1, num2;
-    char operacion;
+ 
+void gotoxy(int x, int y) {
+    COORD coord = { (SHORT)x, (SHORT)y };
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coord);
+}
 
-    cout << "Ingrese el primer número: ";
-    cin >> num1;
+void setColor(int color) {
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),color);
 
-    cout << "Ingrese el segundo número: ";
-    cin >> num2;
+}
+int i;
+int j;
+const int MAX_ALUMNOS = 100;
+const int NUM_EXAMENES = 5;
 
-    cout << "Seleccione la operación (+, -, *, /): ";
-    cin >> operacion;
+int main(){ 
+    
+    string nombres[MAX_ALUMNOS];
+    string apellidos1[MAX_ALUMNOS];
+    string apellidos2[MAX_ALUMNOS];
+    int ciclos[MAX_ALUMNOS];
+    int cedulas[MAX_ALUMNOS];
+    float notas[MAX_ALUMNOS][NUM_EXAMENES];
+    float promedios[MAX_ALUMNOS];
+    bool aprobados[MAX_ALUMNOS];
 
-    switch (operacion) {
-        case '+':
-            
-            break;
-        case '-':
-           
-            break;
-        case '*':
-           
-            break;
-        case '/':
-            if (num2 != 0)
-                
-            else
-                cout << "Error: División por cero." << endl;
-            break;
-        default:
-            cout << "Operación no válida." << endl;
-    }
 
+    cout << "\033[2J\033[H";
+
+    gotoxy(50,0);
+    setColor(3);
+    cout << "Sistema De Calificaciones" << endl;
+    
+    setColor(3);
+    cout << "=======================================================================================================================================" << endl;
+        gotoxy(0,3);
+        setColor(3);
+        cout << "¿cuantos alumnos decea evaluar?";
+        setColor(7);
+        cin >> i;
+        cin.ignore(); // Limpiar el buffer para getline
+
+        for (int j = 0; j < i; j++) {
+            setColor(3);
+            cout << "Nombre del alumno " << (j + 1) << ": ";
+            setColor(7);
+            getline(cin, nombres[j]);
+        }
+
+    system("pause");
+    cout << "\033[2J\033[H";
+    
     return 0;
 }
